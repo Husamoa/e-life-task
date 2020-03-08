c885ef2bbb994f0f31d8af2b27bc6d119f9cf7a6
Husamoa <pawebial@gmail.com>
2020-03-08 23:03:07 +0100

    added dark mode

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import { ThemeContext } from '../contexts/ThemeContext';
 
+const ToDoList = ({ items, deleteItem, addToDone }) => {
+  const { lightTheme } = useContext(ThemeContext);
+  const theme = !lightTheme ? ' darkmode' : '';
 
-const ToDoList = ({ items, deleteItem, addToDone }) => (
-  <ul>
-    <h1 className="todo">TO DO</h1>
-    {items.map((item, index) => (
-      /* eslint-disable */
-      <li onClick={e => deleteItem(index)} key={index}>
-        {item}
-        <button className="left" onClick={e => addToDone(item)}>DONE</button>
-        {' '}
-      </li>
-      /* eslint-enable */
-    ))}
-  </ul>
-);
+  return (
+    <div className={`todolist${theme}`}>
+      <ul className={`container${theme}`}>
+        <h1 className="todo">TO DO</h1>
+        {items.map((item, index) => (
+        /* eslint-disable */
+          <li onClick={e => deleteItem(index)} key={index}>
+            <span className="todo-item">{item}</span>
+            <button className="left" onClick={e => addToDone(item)}>DONE</button>
+            {' '}
+          </li>
+        /* eslint-enable */
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 ToDoList.propTypes = {
   items: PropTypes.instanceOf(Array).isRequired,
---
+++ src/components/ToggleTheme.js
@@ -0,0 +1,31 @@
+import React, { useState, useContext } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
+
+const ToggleTheme = () => {
+  const { toggleTheme } = useContext(ThemeContext);
+  const [icon, setIcon] = useState(true);
+
+  const iconChange = () => {
+    const newIcon = !icon;
+    setIcon(newIcon);
+  };
+
+  return (
+    <div className="toggle__box">
+      <span>
+        {icon ? (
+          <p className="black">Swich to dark mode</p>
+        ) : (
+          <p className="white">Swich to light mode</p>
+        )}
+      </span>
+      <div className="toggle__btn" onClick={toggleTheme}>
+        <input type="checkbox" className="checkbox" onChange={iconChange} />
+        <div className="circle" />
+        <div className="layer" />
+      </div>
+    </div>
+  );
+};
+
+export default ToggleTheme;
